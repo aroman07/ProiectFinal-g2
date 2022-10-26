1840cc4d3ca4c3c42d270f3974f60848b22fda2a
aroman07 <87022612+aroman07@users.noreply.github.com>
2022-10-26 16:56:35 +0300

    Add files via upload

---
+++ src/Main.java
@@ -0,0 +1,15 @@
+import javax.swing.*;
+
+public class Main {
+    JPanel panel;
+
+    public static void main(String[] args) {
+        Main m = new Main();
+        m.panel = new JPanel();
+
+        JFrame frame = new JFrame("App");
+        frame.setContentPane(m.panel);
+        frame.pack();
+        frame.setVisible(true);
+    }
+}
